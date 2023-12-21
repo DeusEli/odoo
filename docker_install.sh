fd04dfa642a334c96a282012b4c73c0fea63b077
DeusEli <54603839+DeusEli@users.noreply.github.com>
2023-12-21 11:46:04 -0600

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-# Actualizar e instalar paquetes
+# Actualizar e instalar paquetes necesarios
 sudo apt update
 sudo apt install -y ca-certificates curl gnupg
+
+# Crear directorio para las claves GPG
 sudo install -y -m 0755 -d /etc/apt/keyrings
 
-# Descargar el archivo docker gpg
+# Descargar el archivo GPG de Docker y colocarlo en el directorio creado
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-# Agregar soporte
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Agregar el repositorio de Docker al archivo sources.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Instalar los paquetes docker
+# Actualizar e instalar Docker
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verificar la instalación exitosa
+if [ $? -eq 0 ]; then
+  echo "Docker se ha instalado correctamente."
+else
+  echo "Hubo un problema durante la instalación de Docker. Por favor, verifica y corrige los errores."
+fi
