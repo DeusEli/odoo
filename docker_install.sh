13baa3c3ac12290e1bd3a0d748551659d846f43e
DeusEli <54603839+DeusEli@users.noreply.github.com>
2023-12-21 14:13:04 -0600

    Fix docker_install.sh
    Added comprobation to keyrings files

--- docker_install.sh
+++ docker_install.sh
@@ -4,15 +4,20 @@
 sudo apt update
 sudo apt install -y ca-certificates curl gnupg
 
-# Crear directorio para las claves GPG
+# Crear directorio para las claves GPG si no existe
 sudo install -m 0755 -d /etc/apt/keyrings
 
-# Descargar el archivo GPG de Docker y colocarlo en el directorio creado
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# Descargar el archivo GPG de Docker solo si no existe
+GPG_FILE="/etc/apt/keyrings/docker.gpg"
+if [ ! -f "$GPG_FILE" ]; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o "$GPG_FILE"
+    sudo chmod a+r "$GPG_FILE"
+else
+    echo "El archivo $GPG_FILE ya existe. Saltando la descarga."
+fi
 
 # Agregar el repositorio de Docker al archivo sources.list
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo bash -c 'cat > /etc/apt/sources.list.d/docker.list'
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Actualizar e instalar Docker
 sudo apt update
