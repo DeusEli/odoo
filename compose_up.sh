20e6f736f0171816b69d7453a09e192ceb44e909
DeusEli <54603839+DeusEli@users.noreply.github.com>
2023-12-21 22:31:09 -0600

    Update compose_up.sh
    Added line to change permission

--- compose_up.sh
+++ compose_up.sh
@@ -5,3 +5,6 @@ export NGINX_SERVER_NAME=$1
 
 # Levantar los contenedores
 docker compose up -d
+
+# Cambiar los permisos de la carpeta /var/lib/odoo
+docker exec -ti -u root odoo16 chown -R odoo:odoo /var/lib/odoo
