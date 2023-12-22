8fa3fe6107bb450de3171788d7e9117a0f0985a9
DeusElí <54603839+DeusEli@users.noreply.github.com>
2023-12-22 15:03:12 -0600

    Update compose_up.sh

--- compose_up.sh
+++ compose_up.sh
@@ -6,6 +6,13 @@ export NGINX_SERVER_NAME=$1
 # Levantar los contenedores
 docker compose up -d
 
+# Esperar a que los contenedores estén en funcionamiento
+echo "Esperando a que los contenedores estén en funcionamiento..."
+while [ "$(docker inspect -f '{{.State.Status}}' odoo16 2>/dev/null)" != "running" ]; do
+    sleep 1
+done
+echo "Los contenedores están en funcionamiento."
+
 # Cambiar los permisos de la carpeta /var/lib/odoo
 if docker exec -ti -u root odoo16 chown -R odoo:odoo /var/lib/odoo; then
     echo "Cambio de permisos exitoso"
